e6ac49167b85e96ae6a2a1cc99e911785d061dde
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-16 18:12:39 +0300

    Update job-updater.groovy

--- job-updater.groovy
+++ job-updater.groovy
@@ -2,15 +2,30 @@ timeout(time: 5, unit: 'MINUTES') {
     node('gradle') {
 
 
-        echo "YAML_CONFIG content: ${env.YAML_CONFIG}"
-        echo "YAML_CONFIG content: ${params.YAML_CONFIG}"
+        // echo "YAML_CONFIG content: ${env.YAML_CONFIG}"
+        // echo "YAML_CONFIG content: ${params.YAML_CONFIG}"
         
-        def config = readYaml env.YAML_CONFIG
+        // def config = readYaml env.YAML_CONFIG
 
         
-        def jenkinsUrl = config['JENKINS_URL']
-        def username = config['JENKINS_USERNAME']
-        def password = config['JENKINS_PASSWORD']
+        // def jenkinsUrl = config['JENKINS_URL']
+        // def username = config['JENKINS_USERNAME']
+        // def password = config['JENKINS_PASSWORD']
+
+        parameters {
+            string(name: 'YAML_CONFIG', defaultValue: '', description: 'YAML configuration content')
+        }
+        
+        stages {
+            stage('Read YAML') {
+                    script {
+                        def config = readYaml text: params.YAML_CONFIG
+                        echo "Jenkins URL: ${config.JENKINS_URL}"
+                        echo "Username: ${config.JENKINS_USERNAME}"
+                        echo "Password: ${config.JENKINS_PASSWORD}"
+                    }
+            }
+        }
 
         stage('checkout') {
             checkout scm
