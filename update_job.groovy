3a7326b8afcb7c92dc46cc6da6219729123d598d
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-16 16:22:25 +0300

    Create update_job.groovy

---
+++ update_job.groovy
@@ -0,0 +1,17 @@
+timeout(5, timeUnit = 'MINUTES') {
+    node('main') {
+
+        def config = readYaml text:$YAML_CONFIG
+        def jenkinsUrl = config['JENKINS_URL']
+        def username = config['JENKINS_USERNAME']
+        def password = config['JENKINS_PASSWORD']
+
+        stage('checkout') {
+            checkout scm
+        }
+
+        stage('Update job') {
+            sh "docker run -t localhost:5005/jenkins_updater $jenkinsUrl $username $password"
+        }
+    }
+}
