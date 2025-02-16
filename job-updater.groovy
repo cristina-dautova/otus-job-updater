88374fc90008c9cb38f09d122e5144d78b892146
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-16 20:59:37 +0300

    Update job-updater.groovy

--- job-updater.groovy
+++ job-updater.groovy
@@ -1,17 +1,6 @@
 timeout(time: 5, unit: 'MINUTES') {
-    node('master') {
-
-
-        // echo "YAML_CONFIG content: ${env.YAML_CONFIG}"
-        // echo "YAML_CONFIG content: ${params.YAML_CONFIG}"
+    node {
         
-        // def config = readYaml env.YAML_CONFIG
-
-        
-        // def jenkinsUrl = config['JENKINS_URL']
-        // def username = config['JENKINS_USERNAME']
-        // def password = config['JENKINS_PASSWORD']
-
         parameters {
             string(name: 'YAML_CONFIG', defaultValue: '', description: 'YAML configuration content')
         }
